60269b36e458ea2e309bc090bb8785d138f1e060
SuperAlexcom <114802210+SuperAlexcom@users.noreply.github.com>
2024-01-08 16:03:00 +0300

    Lab1-4

---
+++ TPP1/TPP1/TPP1.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <omp.h>
+#include <locale.h>
+#include <time.h>
+
+const int NMAX = 500;
+const int LIMIT = 10;
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	double t1, t2, dt;
+	t1 = omp_get_wtime();
+
+	int i, j;
+	float sum;
+	float a[NMAX][NMAX];
+	for (i = 0; i < NMAX; i++)
+		for (j = 0; j < NMAX; j++)
+			a[i][j] = i + j;
+#pragma omp parallel shared(a) if (NMAX>LIMIT)
+	{
+#pragma omp for private(i,j,sum)
+		for (i = 0; i < NMAX; i++)
+		{
+			sum = 0;
+			for (j = 0; j < NMAX; j++)
+				sum += a[i][j];
+			printf("Сумма элементов строки %d равна %f\n", i, sum);
+		}
+	}
+
+	t2 = omp_get_wtime();
+	dt = t2 - t1;
+	printf("Программа работала %f секунд.\n", dt);
+}
---
+++ TPP3/TPP3/TPP3.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <stdio.h>
+#include <clocale>
+
+const int NMAX = 10;
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	int i, j;
+	long long int total = 0;
+	long long int sum = 0;
+	int** A = new int* [NMAX];
+	for (i = 0; i < NMAX; ++i) {
+		A[i] = new int[NMAX];
+	}
+
+	int** B = new int* [NMAX];
+	for (i = 0; i < NMAX; ++i) {
+		B[i] = new int[NMAX];
+	}
+
+	int** C = new int* [NMAX];
+	for (i = 0; i < NMAX; ++i) {
+		C[i] = new int[NMAX];
+	}
+
+	for (i = 0; i < NMAX; i++)
+		for (j = 0; j < NMAX; j++)
+		{
+			A[i][j] = (i + j) % 10;
+			B[i][j] = (i + j + 1) % 10;
+			C[i][j] = (i + j + 2) % 10;
+		}
+	double start_time = omp_get_wtime();
+#pragma omp parallel shared(A,B,C, total) 
+	{
+#pragma omp for  lastprivate(i, j, sum)
+		for (i = 0; i < NMAX; i++)
+		{
+			sum = 0;
+			for (j = 0; j < NMAX; j++)
+			{
+				if (A[i][j] % 2 == 0) sum += B[i][j] + C[i][j];
+				else sum += B[i][j] - A[i][j];
+			}
+			//printf("Сумма элементов %d строки равна: %d\n", i, sum);
+#pragma omp critical
+			total += sum;
+		}
+	}
+	printf("\nsum: %d", total);
+	printf("\nВремя: %lf\n", omp_get_wtime() - start_time);
+
+	total = 0;
+	start_time = omp_get_wtime();
+#pragma omp parallel shared(A,B,C, total) 
+	{
+#pragma omp for  lastprivate(i, j, sum)
+		for (i = 0; i < NMAX; i++)
+		{
+			sum = 0;
+			for (j = 0; j < NMAX; j++)
+			{
+				if (A[i][j] % 2 == 0) sum += B[i][j] + C[i][j];
+				else sum += B[i][j] - A[i][j];
+			}
+			//printf("Сумма элементов %d строки равна: %d\n", i, sum);
+#pragma omp atomic
+			total += sum;
+		}
+	}
+	printf("\nsum: %d", total);
+	printf("\nВремя: %lf", omp_get_wtime() - start_time);
+
+	for (int i = 0; i < NMAX; ++i) delete[] A[i], B[i], C[i];
+	delete[] A, B, C;
+}
